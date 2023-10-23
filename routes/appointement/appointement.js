857f5c77b6a29ae4e4d52b5c360a6fc4a0d76cf2
MatteoFar <matteo.farigu@gmail.com>
2023-10-23 14:22:16 +0200

    adding post appoitement

--- routes/appointement/appointement.js
+++ routes/appointement/appointement.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 
 import { poolPromise } from "../../config/db.js";
-import { getIncomingAppointement } from "./function.js";
+import { getIncomingAppointement, postAppoitement } from "./function.js";
 
 dotenv.config()
 
@@ -12,9 +12,18 @@ router.get("/appointements/:users_id", async (req, res) => {
     try {
         const p = await getIncomingAppointement(req.params)
         console.log('pppppp',p)
-        res.status(200).send('test')
+        res.status(200).send({data : p})
     } catch (error) {
-        console.error(error)
+        res.status(500).send({type: error.status, message: error.message})
+    }
+})
+router.post("/appointements", async (req,res) => {
+    try {
+        const p = await postAppoitement(req.body)
+        console.log('pppppp', p)
+        res.status(200).send('rendez-vous posté !')
+    } catch (error) {
+        res.status(500).send({type: error.status, message: error.message})
     }
 })
 
--- routes/appointement/function.js
+++ routes/appointement/function.js
@@ -3,6 +3,31 @@ import { poolPromise } from "../../config/db.js";
 import jwt from "jsonwebtoken"
 import moment from "moment";
 
+/// post Appointement
+export async function postAppoitement(data) {
+    try {
+        const dataAppointement = postAppoitementUser(data)
+        return dataAppointement
+    } catch (error) {
+        console.error('err',error)
+        throw error
+    }
+}
+
+export async function postAppoitementUser(data) {
+    try {
+        const {date_meeting, reason, created_at, Id_office, Id_Doctor, Id_Users} = data
+        const query = 'INSERT INTO appointement (date_meeting, validation, reason, created_at, Id_office, Id_Doctor, Id_Users) value (?, 0, ?, ?, ?, ?, ?)'
+        const result = await poolPromise.query(query, [date_meeting, reason, created_at, Id_office, Id_Doctor, Id_Users]);
+
+        return result[0]
+    } catch (error) {
+        console.error('err',error)
+        throw error
+    }
+}
+
+/// get Appointement
 export async function getIncomingAppointement(data) {
     try {
         const dataAppointement = getIncomingAppointementUser(data)
@@ -15,9 +40,9 @@ export async function getIncomingAppointement(data) {
 
 export async function getIncomingAppointementUser(data) {
     try {
-        let query = 'SELECT * from appointement where Id_Users=?'
+        const query = 'SELECT * from appointement where Id_Users=?'
 
-        let {users_id} = data
+        const {users_id} = data
 
         const result = await poolPromise.query(query, [users_id]);
 
